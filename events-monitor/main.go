2442d8a65ab2c97376304fcfacbb2f1fca14b89a
Raul Jordan <raul@prysmaticlabs.com>
2022-08-25 12:16:52 -0400

    smtp sender

--- events-monitor/main.go
+++ events-monitor/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/smtp"
 	"os"
+	"time"
 
 	"github.com/prysmaticlabs/prysm/v3/proto/eth/service"
 	v1 "github.com/prysmaticlabs/prysm/v3/proto/eth/v1"
@@ -15,6 +19,11 @@ import (
 var (
 	monitorFlags = struct {
 		beaconEndpoint string
+		sendTo         string
+		smtpHost       string
+		smtpPort       string
+		smtpPassword   string
+		smtpUsername   string
 		topics         cli.StringSlice
 	}{}
 )
@@ -28,8 +37,9 @@ func init() {
 
 func main() {
 	app := &cli.App{
-		Name:  "events-monitor",
-		Usage: "Ethereum beacon node events monitoring tool",
+		Name: "events-monitor",
+		Usage: "Ethereum beacon node events monitoring tool with the ability to send emails " +
+			"with the event data as JSON attachments",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:        "grpc-endpoint",
@@ -43,9 +53,42 @@ func main() {
 				Usage:       "List of event topics to subscribe to",
 				Required:    true,
 			},
+			&cli.StringFlag{
+				Name:        "send-to",
+				Destination: &monitorFlags.sendTo,
+				Usage:       "Recipient email address for events",
+			},
+			&cli.StringFlag{
+				Name:        "smtp-host",
+				Destination: &monitorFlags.smtpHost,
+				Usage:       "Smtp host for sending emails",
+			},
+			&cli.StringFlag{
+				Name:        "smtp-username",
+				Destination: &monitorFlags.smtpUsername,
+				Usage:       "Smtp username for sending emails",
+			},
+			&cli.StringFlag{
+				Name:        "smtp-port",
+				Destination: &monitorFlags.smtpPort,
+				Usage:       "Smtp port for sending emails",
+			},
+			&cli.StringFlag{
+				Name: "smtp-password-file",
+				// TODO: Ensure it is passed in as a file instead.
+				Destination: &monitorFlags.smtpPassword,
+				Usage:       "Smtp password for sending emails",
+			},
 		},
 		Action: func(cliCtx *cli.Context) error {
-			return monitorEvents(cliCtx.Context)
+			auth := smtp.PlainAuth(
+				"",
+				monitorFlags.smtpUsername,
+				monitorFlags.smtpPassword,
+				monitorFlags.smtpHost,
+			)
+			sender := newBasicSmtpSender(auth, "25" /* port number */)
+			return monitorEvents(cliCtx.Context, sender)
 		},
 	}
 
@@ -54,7 +97,7 @@ func main() {
 	}
 }
 
-func monitorEvents(ctx context.Context) error {
+func monitorEvents(ctx context.Context, sender emailSender) error {
 	conn, err := grpc.Dial(monitorFlags.beaconEndpoint, grpc.WithInsecure())
 	if err != nil {
 		return err
@@ -82,6 +125,20 @@ func monitorEvents(ctx context.Context) error {
 				return err
 			}
 			log.WithField("head", ev).Info("Received event")
+			m := newEmailMessage(
+				"New Head Event",
+				fmt.Sprintf("Received a new blockchain head event at %v\n", time.Now()),
+			)
+			m.to = []string{monitorFlags.sendTo}
+			rawEvent, err := json.Marshal(ev)
+			if err != nil {
+				log.WithError(err).Error("Could marshal head event")
+				continue
+			}
+			m.attachFileBytes("head.json", rawEvent)
+			if err := sender.send(m); err != nil {
+				log.WithError(err).Error("Could not send head event as email attachment")
+			}
 		case "block":
 			ev := &v1.EventBlock{}
 			if err := data.Data.UnmarshalTo(ev); err != nil {
