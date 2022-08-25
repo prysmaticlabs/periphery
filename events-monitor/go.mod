447996fb452ea0d52e001f7125e32b76fe96a21b
Raul Jordan <raul@prysmaticlabs.com>
2022-08-25 11:54:30 -0400

    event process

--- events-monitor/go.mod
+++ events-monitor/go.mod
@@ -3,9 +3,41 @@ module github.com/prysmaticlabs/periphery/events-monitor
 go 1.18
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/prysmaticlabs/prysm/v3 v3.0.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.11.2 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	github.com/prysmaticlabs/prysm/v3 v3.0.0
+	github.com/sirupsen/logrus v1.9.0
+	github.com/urfave/cli/v2 v2.11.2
+	google.golang.org/grpc v1.40.0
 )
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.20 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prysmaticlabs/fastssz v0.0.0-20220628121656-93dfe28febab // indirect
+	github.com/prysmaticlabs/go-bitfield v0.0.0-20210809151128-385d8c5e3fb7 // indirect
+	github.com/prysmaticlabs/gohashtree v0.0.2-alpha // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/thomaso-mirodin/intmath v0.0.0-20160323211736-5dc6d854e46e // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210426193834-eac7f76ac494 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+// See https://github.com/prysmaticlabs/grpc-gateway/issues/2
+replace github.com/grpc-ecosystem/grpc-gateway/v2 => github.com/prysmaticlabs/grpc-gateway/v2 v2.3.1-0.20220721162526-0d1c40b5f064
--- events-monitor/main.go
+++ events-monitor/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"log"
+	"context"
 	"os"
 
 	"github.com/prysmaticlabs/prysm/v3/proto/eth/service"
+	v1 "github.com/prysmaticlabs/prysm/v3/proto/eth/v1"
+	prefixed "github.com/prysmaticlabs/prysm/v3/runtime/logging/logrus-prefixed-formatter"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 	"google.golang.org/grpc"
 )
@@ -12,8 +15,16 @@ import (
 var (
 	monitorFlags = struct {
 		beaconEndpoint string
+		topics         cli.StringSlice
 	}{}
 )
+
+func init() {
+	formatter := new(prefixed.TextFormatter)
+	formatter.TimestampFormat = "2006-01-02 15:04:05"
+	formatter.FullTimestamp = true
+	log.SetFormatter(formatter)
+}
 
 func main() {
 	app := &cli.App{
@@ -26,9 +37,15 @@ func main() {
 				Value:       "localhost:4000",
 				Usage:       "gRPC endpoint for an Ethereum beacon node",
 			},
+			&cli.StringSliceFlag{
+				Name:        "topics",
+				Destination: &monitorFlags.topics,
+				Usage:       "List of event topics to subscribe to",
+				Required:    true,
+			},
 		},
-		Action: func(*cli.Context) error {
-			return monitorEvents()
+		Action: func(cliCtx *cli.Context) error {
+			return monitorEvents(cliCtx.Context)
 		},
 	}
 
@@ -37,11 +54,52 @@ func main() {
 	}
 }
 
-func monitorEvents() error {
+func monitorEvents(ctx context.Context) error {
 	conn, err := grpc.Dial(monitorFlags.beaconEndpoint, grpc.WithInsecure())
 	if err != nil {
 		return err
 	}
 	eventsClient := service.NewEventsClient(conn)
-	return nil
+	recv, err := eventsClient.StreamEvents(ctx, &v1.StreamEventsRequest{
+		Topics: monitorFlags.topics.Value(),
+	})
+	if err != nil {
+		return err
+	}
+	for {
+		data, err := recv.Recv()
+		if err != nil {
+			return err
+		}
+		if data == nil {
+			log.Error("Received nil event data from beacon node")
+			continue
+		}
+		switch data.Event {
+		case "head":
+			ev := &v1.EventHead{}
+			if err := data.Data.UnmarshalTo(ev); err != nil {
+				return err
+			}
+			log.WithField("head", ev).Info("Received event")
+		case "block":
+			ev := &v1.EventBlock{}
+			if err := data.Data.UnmarshalTo(ev); err != nil {
+				return err
+			}
+			log.WithField("block", ev).Info("Received event")
+		case "finalized_checkpoint":
+			ev := &v1.EventFinalizedCheckpoint{}
+			if err := data.Data.UnmarshalTo(ev); err != nil {
+				return err
+			}
+			log.WithField("finalized_checkpoint", ev).Info("Received event")
+		case "reorg":
+			ev := &v1.EventChainReorg{}
+			if err := data.Data.UnmarshalTo(ev); err != nil {
+				return err
+			}
+			log.WithField("chain_reorg", ev).Info("Received event")
+		}
+	}
 }
